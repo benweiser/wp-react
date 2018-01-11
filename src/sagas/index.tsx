b5dd00d23ab1b106a13222c2283535e5f018d49a
Ben Weiser <bweiser@caesars.com>
2018-01-10 17:14:24 -0800

    Redux sagas work woohooo!

--- src/sagas/index.tsx
+++ src/sagas/index.tsx
@@ -1,20 +1,18 @@
 import { delay } from "redux-saga";
-import { all, put, takeEvery } from "redux-saga/effects";
-import { INCREMENT_ENTHUSIASM } from "../constants/index";
-import * as constants from "../constants";
+import {all, call, put, takeEvery} from "redux-saga/effects";
+import { INCREMENT_ENTHUSIASM , ASYNC_INCREMENT_ENTHUSIASM} from "../constants/index";
 
 export function* helloSaga(): IterableIterator<{}> {
   console.log("Hello Sagas!");
 }
 
 export function* incrementAsync(): IterableIterator<{}> {
-  console.log("async stuff", yield delay(1000));
-  yield delay(500000);
-  yield put({ type: constants.ASYNC_INCREMENT_ENTHUSIASM });
+    yield call(delay, 5000);
+    yield put({ type: INCREMENT_ENTHUSIASM });
 }
 
 export function* watchIncrementAsync(): IterableIterator<{}> {
-  yield takeEvery(INCREMENT_ENTHUSIASM, incrementAsync);
+  yield takeEvery(ASYNC_INCREMENT_ENTHUSIASM, incrementAsync);
 }
 
 export default function* rootSaga() {
@@ -23,16 +21,3 @@ export default function* rootSaga() {
      watchIncrementAsync()
   ]);
 }
-
-function* idMaker() {
-  var index = 0;
-  while (index < 3)
-    yield index++;
-}
-
-var gen = idMaker();
-
-console.log(gen.next().value); // 0
-console.log(gen.next().value); // 1
-console.log(gen.next().value); // 2
-console.log(gen.next().value); // undefined
