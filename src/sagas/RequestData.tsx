b959142017ef10a29959daad56f5f3d1d4c09ec5
benweiser <bweiser86@gmail.com>
2018-01-15 02:31:12 -0800

    Refactor out sagas into their own files where it makes logical sense

--- src/sagas/RequestData.tsx
+++ src/sagas/RequestData.tsx
@@ -6,8 +6,8 @@ import { BASE_URI } from "../constants/index";
 const makeRequest = async (query: string = "posts") => {
   try {
     return await axios
-        .get(`${BASE_URI}${query}`)
-        .then(response => response.data.map((post: PostItem) => new Post(post)));
+      .get(`${BASE_URI}${query}`)
+      .then(response => response.data.map((post: PostItem) => new Post(post)));
   } catch (e) {
     console.log(e);
   }
