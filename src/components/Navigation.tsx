572415af5f951a0e124d7c2bb3c2c6a4270240d5
benweiser <bweiser86@gmail.com>
2018-04-11 22:06:26 -0700

    Modified navigation component

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,3 +1,4 @@
+import { ROUTES } from "../Routes";
 import styled from "../styles/styled-components";
 import * as React from "react";
 
@@ -5,24 +6,28 @@ import { space, alignItems, display } from "styled-system";
 import { StyledNavItem } from "./atoms/NavItem/StyledNavItem";
 
 interface StyledNavProps {
-  className?: string;
+  readonly className?: string;
 }
 
 const Navigation: React.StatelessComponent<StyledNavProps> = ({
   className
-}: StyledNavProps) => (
-  <nav className={className}>
-    <StyledNavItem p={3} display="block" to="/test">
-      Test
-    </StyledNavItem>
-    <StyledNavItem p={3} display="block" to="/blog">
-      Blog
-    </StyledNavItem>
-    <StyledNavItem p={3} display="block" to="/blog">
-      Blog
-    </StyledNavItem>
-  </nav>
-);
+}: StyledNavProps) => {
+  return (
+    <nav className={className}>
+      {ROUTES.map((route, index) => (
+        <StyledNavItem
+          p={3}
+          display="block"
+          alignItems="center"
+          key={index}
+          to={`/${route.path}`}
+        >
+          {route.name}
+        </StyledNavItem>
+      ))}
+    </nav>
+  );
+};
 
 const StyledNavigation = styled(Navigation)`
   ${space};
